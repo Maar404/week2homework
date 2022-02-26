ae123eb5f81dfec9af88a425ba3b6119def1b7f6
Maar404 <100438123+Maar404@users.noreply.github.com>
2022-02-27 00:06:38 +0330

    Update shpaesMain.java

--- shpaesMain.java
+++ shpaesMain.java
@@ -2,7 +2,7 @@ package com.company;
 
 import java.util.Scanner;
 
-public class Main {
+public class shpaesMain {
 
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
